bcc661d556cb554a64d01c66894e811f0d3d50e2
Lorena Pantano <lorena.pantano@gmail.com>
2017-05-02 13:58:05 -0400

    Add travis

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+mkdir -p $PREFIX/custom
+ln -s /System/Library/Frameworks/OpenGL.framework/Libraries $PREFIX/custom/lib
+ln -s /System/Library/Frameworks/OpenGL.framework/Headers $PREFIX/custom/GL
+
+export GL_CFLAGS="-I$PREFIX/custom"
+export GL_LIBS="-I$PREFIX/custom/lib"
 export CFLAGS="-I${PREFIX}/include"
 export LDFLAGS="-L${PREFIX}/lib"
 ./configure  --prefix=$PREFIX ##--disable-static
